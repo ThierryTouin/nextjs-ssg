13f6a3d45489217fceff771434bf46c0ddf68353
Thierry <thierrytouin@gmail.com>
2025-03-01 23:35:51 +0100

    test ssg

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export", // 🚀 Active le mode statique
+  output: "export", // 🚀 Active le mode statique / Permet de générer un site statique
+  distDir: 'docs', // Change le dossier de sortie (au lieu de "out")
   images: {
     unoptimized: true, // ⚠️ Obligatoire si tu utilises next/image en mode statique
   }
