b107889ddee7acc6fa780dc20f821beb8ff3bca9
Thierry <thierrytouin@gmail.com>
2025-03-01 23:21:41 +0100

    first commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export", // 🚀 Active le mode statique
+  images: {
+    unoptimized: true, // ⚠️ Obligatoire si tu utilises next/image en mode statique
+  }
 };
 
 export default nextConfig;
