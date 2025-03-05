a4de36e82ee1885a35e33384886b474c692b5a37
Thierry <thierrytouin@gmail.com>
2025-03-05 23:41:22 +0100

    test suivant

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
+//const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
+const basePath = '/nextjs-ssg';
 
 const nextConfig: NextConfig = {
   /* config options here */
