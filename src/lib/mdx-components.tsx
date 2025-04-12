90320437b29f23ad3e8728e803a06a7c124cbc44
Thierry <thierrytouin@gmail.com>
2025-04-12 23:50:13 +0200

    test

--- src/lib/mdx-components.tsx
+++ src/lib/mdx-components.tsx
@@ -1,6 +1,6 @@
 // lib/mdx-components.tsx
 import Card from '@/components/Card'
-import type { MDXComponents } from 'mdx/types.js'
+import type { MDXComponents } from 'mdx/types'
 
 export const mdxComponents: MDXComponents = {
   Card, // rend le composant Card utilisable dans les fichiers .mdx
